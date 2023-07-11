9590cb553511463fe71546019a12a8b1c10438a9
ahmad90384 <139243964+ahmad90384@users.noreply.github.com>
2023-07-11 19:19:22 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,9 @@
-// const express = require("express");
-// const app = express();
-// const port = process.env.PORT || 5000;
-// const dotenv = require("dotenv");
-// const cors = require("cors"); // Import the cors package
-
-// dotenv.config();
-// dotenv.config({ path: "./config.env" });
-// require("./dbConnection");
-
-// app.use(express.json());
-// app.use(cors()); // Use the cors middleware
-
-// app.use(require("./router/fetchInfo"));
-
-// app.get("/", (req, res) => {
-//   res.send("Hello world");
-// });
-
-// app.listen(port, () => {
-//   console.log(`Listening on port ${port}`);
-// });
-
-// development changes
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const dotenv = require("dotenv");
 const cors = require("cors"); // Import the cors package
+const path = require("path");
 
 dotenv.config();
 dotenv.config({ path: "./config.env" });
